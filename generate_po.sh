a05b274f097355d69a3c918967bc651c9e607b95
Jonghyun Park <adrysn@gmail.com>
2020-03-30 15:05:50 +0900

    fix: remove unneeded file

--- generate_po.sh
+++
@@ -1,7 +0,0 @@
-# Extract translatable messages into .pot files
-make gettext
-
-# Generate .po files. Generated .po files will be placed:
-sphinx-intl update -p _build/gettext -l ko
-
-# Now, translate with .po files
