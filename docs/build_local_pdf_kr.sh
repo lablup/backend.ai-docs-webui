f54ebcd08303fd3ea273afb8d5a875546e545041
Jonghyun Park <jpark@lablup.com>
2022-05-24 17:50:20 +0900

    Bump version to 22.03.1

--- docs/build_local_pdf_kr.sh
+++ docs/build_local_pdf_kr.sh
@@ -1,4 +1,5 @@
 \cp -r conf.py conf.original.py
 \cp -r conf.local.py conf.py
-make -e SPHINXOPTS="-D language='ko'" latexpdf
+# make -e SPHINXOPTS="-D language='ko'" latexpdf
+make latexpdf
 \mv -f conf.original.py conf.py
